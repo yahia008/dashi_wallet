16cfd060eb783179774e5add0df7eb8a6c291b5d
yahia008 <yahyatijjani99@jmail.com>
2025-07-02 10:53:16 +0100

    controller

--- dashi_wallet/src/contribution/contribution.controller.ts
+++ dashi_wallet/src/contribution/contribution.controller.ts
@@ -36,46 +36,6 @@ export class ContributionController {
     return { link: paymentLink }
   }
 
-    @Post('payment/webhook')
-  async handleWebhook(@Req() req) {
-    const payload = req.body;
-
-    if (
-      payload.event === 'charge.completed' &&
-      payload.data.status === 'successful'
-    ) {
-      const { id: transactionId, tx_ref, amount, customer } = payload.data;
-      const email = customer.email;
-      console.log('🔥 Received webhook from Flutterwave:');
-      console.log(JSON.stringify(req.body, null, 2))
-
-      return await this.paymentService.transaction(amount, tx_ref, email, transactionId)
-
-      
-    }
-
-    return { message: 'Webhook received but ignored' };
-  }
-
-   @Get('verify')
-  async verifyPayment(
-    @Query('transaction_id') transaction_id: string,
-    @Query('status') status: string,
-    @Query('tx_ref') tx_ref: string,
-    @Res() res,
-    @Req() req
-  ) {
-    const email = req.user.email
-    
-      if (!transaction_id || status !== 'successful') {
-        return res.status(400).send('Invalid or failed transaction.');
-      }
-
-      // 1. Verify with Flutterwave
-      const transaction = await this.paymentService.verifyTransaction(transaction_id);
-      
-    return  await this.paymentService.transaction(transaction.amount, tx_ref, email, transaction_id)  
-
-  }
+  
 
 }
